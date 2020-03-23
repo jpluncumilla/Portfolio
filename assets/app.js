5d8a05a76155c7434ebe310d73ccd0c03a28af77
Juan Pablo <jpluncumilla@gmail.com>
2020-03-22 21:36:02 -0400

    added button to go back up and removed softscroll

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,21 @@
-var slideDown = {
-    distance: '100%',
-    origin: 'bottom',
-    opacity: 0.1,
-    delay: 20,
-    duration: 1200,
-};
 
-ScrollReveal().reveal('.fullDoc', slideDown);
+
+//Get the button:
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function(){ scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
